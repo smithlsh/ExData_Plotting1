f838a5f0fd6af82cf58465056c56210b8dbe236f
smithlsh <shep@usinternet.com>
2014-05-11 19:22:20 -0400

    Modified comments slightly

--- plot2.R
+++ plot2.R
@@ -8,7 +8,7 @@ data$Global_active_power<-as.numeric(data$Global_active_power)
 # Pick out the relvant dates over which we will compute the hisgtogram
 subData<-data[data$Date>=as.Date("2007-02-01") &data$Date<=as.Date("2007-02-02"),]
 
-# Plot and dave graph as png file
+# Plot and save graph as png file
 png(filename="plot2.png", width=480, height=480, units="px")
 plot(subData$Global_active_power, type="l", xlab="",xaxt="n",ylab="Global Active Power (kilowatts)")
 axis(1, at=c(1, as.integer(nrow(subData)/2), nrow(subData)), labels=c("Thu", "Fri", "Sat"))
